4432e2b8f56d5f649119a80311e0b45cf848db8a
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-04-07 15:50:48 -0400

    feat: add scores to player serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :username, :password
+
+  has_many :scores
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ difficulty3 = Difficulty.create(difficulty: 'hard', player_id: player1.id)
 
  #victory is based on whether they got a higher score than opponent, null when solo
 score1 = Score.create(score: 10, difficulty_id: difficulty1.id)
-score2 = Score.create(score: 10, difficulty_id: difficulty2.id)
-score3 = Score.create(score: 10, difficulty_id: difficulty3.id)
+score2 = Score.create(score: 9, difficulty_id: difficulty2.id)
+score3 = Score.create(score: 5, difficulty_id: difficulty3.id)
 
 puts 'done seeding! 🌱🌱🌱'
